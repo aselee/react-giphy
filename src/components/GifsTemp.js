125f6e5d3aaa42c234653ff951681086898eaf05
Andrew <32229265+aselee@users.noreply.github.com>
2019-01-20 18:09:50 -0600

    adding temp gif boilerplate

---
+++ src/components/GifsTemp.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// combined temp code of GifList and GifItem
+
+// Having ({gifs}) as a parameter,
+// means that this function is expecting an object
+// passed in a property of gifs.
+// Rather than passing in an object with that property,
+// and then accessing gifs in our functino as objectNameHere.gifs
+// this syntax allows to pull out the gifs property of any object 
+// that is passed in, and automatically assigns it to a variable,
+// named gifs. 
+// If passing {title: 'stuff', gifs:'abc} into this function,
+// the function would have a gifs variable available to it that was equal to 'abc'.
+const GifsTemp = ( {gifs} ) => {
+  const gifItems = gifs.map ((gif) => {
+    return(
+      <li key={gif.id}>
+        <img src={gif.url} alt="gifURL"/>
+      </li>
+    );
+  });
+
+  return (
+    <ul className="gif-list">
+      {gifItems}
+    </ul>
+  );
+};
