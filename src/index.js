f5b78893cfc930799a9047828d51b7bab1ecf36a
Andrew <32229265+aselee@users.noreply.github.com>
2019-01-20 18:11:51 -0600

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './containers/App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// creating store by calling the
+// configureStore() method that was set up in configureStore.js
+const store = configureStore();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// wrapping App container in a redux Provider.
+// this connects react to the redux store
+
+// using ReactDOM to render the application within
+// the div with an id of app in the index.html
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app') // div id in index.html
+);
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// serviceWorker.unregister();
