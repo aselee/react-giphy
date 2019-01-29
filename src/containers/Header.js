12d15419e9c9afed300d24d91b7bbfcb89f46040
Andrew <32229265+aselee@users.noreply.github.com>
2019-01-29 00:19:26 -0600

    removed a tag and using Link

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 
 // Bootstrap navbar 
+// Using Link to navigate through the app instantly
+// Replacing <a> tags with <Link>, let react-router-dom know it should 
+// swap out the component passed into the App instead of refreshing the page.
 
 class Header extends React.Component {
   render() {
@@ -9,14 +14,17 @@ class Header extends React.Component {
       <nav className="navbar navbar-default">
         <div className="container-fluid">
           <div className="navbar-header">
-            <a href="/" className="navbar-brand">ReactGiphyy</a>
+            {/* <a href="/" className="navbar-brand">ReactGiphyy</a> */}
+            <Link className="navbar-brand" to="/">ReactGiphySearch</Link>
           </div>
           <ul className="nav navbar-nav navbar-right">
             <li className="nav-item">
-              <a className="nav-link" href="/login">Login Here</a>
+              {/* <a className="nav-link" href="/login">Login Here</a> */}
+              <Link ClassName="nav-link" to="/login">Login Here</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/signup">Sign in</a>
+              {/* <a className="nav-link" href="/signup">Sign in</a> */}
+              <Link className="nav-link" to="/signup">Sign in</Link>
             </li>
           </ul>
         </div>
