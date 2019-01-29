668f4f56cfc2857f0d3199c97fb8eccfd30236f7
Andrew <32229265+aselee@users.noreply.github.com>
2019-01-28 19:14:25 -0600

    ConnectedRouter issue

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ConnectedRouter } from 'react-router-redux';
+import { Route } from 'react-router-dom';
+import { history } from './../store/configureStore';
+
+// Routing all the containers
+import Header from '../containers/Header';
+import Home from '../containers/Home';
+import Signup from '../containers/Signup';
+import Login from '../containers/Login';
+import Favorites from '../containers/Favorites';
+
+export default class App extends React.Component {
+  
+  render() {
+    return (
+      // wrapping all of the code in ConnectedRouter component 
+      // from react-router-redux and passing the history object.
+      // ConnectedRouter based on the end of the app's URL.
+      // For ex, url/favorites and uses it as a path to render the components.
+      <ConnectedRouter history={history}>
+        <div>
+          {/* Header will be rendered in every page */}
+          <Header />
+          {/* Can add a Footer here to render on every page */}
+          <div className="container">
+          {/* passing in the component that should render 
+          whenever a path contains exactly "/:" */}
+            <Route exact path="/" component={ Home }/>
+            <Route path="/signup" component={ Signup }/>
+            <Route path="/login" component={ Login }/>
+            <Route path="/favorites" component={ Favorites }/>
+          </div>
+        </div>
+      </ConnectedRouter>
+    );
+  }
+}
+
