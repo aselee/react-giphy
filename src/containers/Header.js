df80649f8f6977c261388b9f884a5b7107e7d0d1
Andrew <32229265+aselee@users.noreply.github.com>
2019-02-01 19:57:35 -0600

    added link pathway and authenication

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import * as Actions from '../actions';
+
 
 // Bootstrap navbar 
 // Using Link to navigate through the app instantly
@@ -9,6 +11,51 @@ import { Link } from 'react-router-dom';
 // swap out the component passed into the App instead of refreshing the page.
 
 class Header extends React.Component {
+
+  // Don't need to call mapsDispatchToProps and bindActionCreators to hook the action creators into an container.
+  // The header doesn't have any child components, so we can just pass in Actions
+  // directly in order to make signOutUser() available on this.props.
+  handleSignout() {
+    this.props.signOutUser();
+  }
+
+
+  // Extracting away the conditional logic needed to redner the links into its own method.
+  // authenticated is being made available from the AuthReducer via mapStateToProps,
+  // and if it's set to true, it will return the links to "My Favs" and "Sign Out", 
+  // since that's what signed-in users should see.
+
+  // Returning with an array of <li>, since any sort of React render() method,
+  // we have to return a single element, which is why we're always wrapping things in <div> tags.
+  // However wrapping a group of <li>s within a <div> inside of a <ul> isn't very clean.
+  // Can instead return an array of comma-separated <li> and react will list them in order.
+  // The only thing that is needed in order to do this, is to give the lists a key prop, so they are unique.
+  renderAuthLinks() {
+    if(this.props.authenticated) {
+      return [
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/favorites">My Favs</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          {/* The reason that the "Sign Out" link is not a <Link>, is because we're not actually
+          routing anywhere when the link is clicked; instead, we're calling the handleSignout() method
+          to fire the signOutUser() action creator. */}
+          <a className="nav-link" href="/" onClick={() => this.handleSignout()}>Sign Out</a>
+        </li>
+      ]
+    } else {
+      return [
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/login">Login</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/signup">Sign Up</Link>
+        </li>
+      ]
+    }
+  }
+
+  
   render() {
     return (
       <nav className="navbar navbar-default">
@@ -18,14 +65,16 @@ class Header extends React.Component {
             <Link className="navbar-brand" to="/">ReactGiphySearch</Link>
           </div>
           <ul className="nav navbar-nav navbar-right">
-            <li className="nav-item">
+
+            {this.renderAuthLinks()}
+            {/* <li className="nav-item"> */}
               {/* <a className="nav-link" href="/login">Login Here</a> */}
-              <Link ClassName="nav-link" to="/login">Login Here</Link>
-            </li>
-            <li className="nav-item">
+              {/* <Link ClassName="nav-link" to="/login">Login Here</Link> */}
+            {/* </li> */}
+            {/* <li className="nav-item"> */}
               {/* <a className="nav-link" href="/signup">Sign in</a> */}
-              <Link className="nav-link" to="/signup">Sign in</Link>
-            </li>
+              {/* <Link className="nav-link" to="/signup">Sign in</Link> */}
+            {/* </li> */}
           </ul>
         </div>
       </nav>
@@ -34,7 +83,9 @@ class Header extends React.Component {
 }
 
 function mapStateToProps(state) {
-  return {}
+  return {
+    authenticated: state.auth.authenticated
+  }
 }
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, Actions)(Header);
