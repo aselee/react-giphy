8ea4b36ceaedc48ff261699a41d6b27ca39fb491
Andrew <32229265+aselee@users.noreply.github.com>
2019-02-02 19:21:05 -0600

    setting up firebase

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 import request from 'superagent';
 
+import Firebase from 'firebase';
+
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 
@@ -10,16 +12,53 @@ export const REQUEST_GIFS = 'REQUEST_GIFS';
 
 // Action types as constants, passing to our action creators
 // signInUser() and signOutUser() via required type property
-export const SIGN_IN_USER = 'SIGN_IN_USER';
+// export const SIGN_IN_USER = 'SIGN_IN_USER';
 export const SIGN_OUT_USER = 'SIGN_OUT_USER';
+
+// For Firebase
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const AUTH_USER = 'AUTH_USER';
 
 const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
 const API_KEY = '&api_key=dc6zaTOxFJmzC'; 
 
+// from Firebase console, importing Firebase package,
+const config = {
+  apiKey: "AIzaSyBtR3H7Ug-luHa1qzxS66lLddDo9JWcouU",
+  authDomain: "react-giphy-a6b0b.firebaseapp.com",
+  databaseURL: "https://react-giphy-a6b0b.firebaseio.com",
+  projectId: "react-giphy-a6b0b",
+  storageBucket: "react-giphy-a6b0b.appspot.com",
+  messagingSenderId: "1045369758402"
+};
+
+Firebase.initializeApp(config);
+
+
+
 // Action Creator:
 // Action creators are functions that create actions.
+
+// ReduxThunk, is forcing the action creator to hold off,
+// on actually dispatching the action object to the reducers
+// until the dispatch is called. 
+// Although the superagent is still returning a promise,
+// the .then(response => {}) resolves that promise,
+// giving the actual result of the call.
+// It is within that method that we dispatch our action with the resolved data. 
 export function requestGifs(term = null) {
-  const data = request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`);
+  return function(dispatch) {
+    request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`).then(response => {
+      dispatch({
+        type: REQUEST_GIFS,
+        payload: response
+      });
+    });
+  }
+}
+
+
+  // const data = request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`);
 
   // console.log(term);
 
@@ -27,12 +66,14 @@ export function requestGifs(term = null) {
   // Plain JavaScript object with two pieces:
   // a type (required) and a payload (only required if,
   // you want to pass data along with your action)
-  return {
-    type: REQUEST_GIFS,
-    payload: data
+
+  // BEFORE:
+  // return {
+  //   type: REQUEST_GIFS,
+  //   payload: data
     // term
-  }
-}
+//   }
+// }
 
 
 // Adding OPEN_MODAL and CLOSE_MODAL with two action creators, openModal() and closeModal().
@@ -51,17 +92,72 @@ export function closeModal() {
   }
 }
 
+//Action types
+
+// Firebase functions; using redux-thunk
+// Both functions are similar but the only difference is between
+// createUserWithEmailAndPassword and signInWithEmailAndPassword.
+
+// createUserWithEmailAndPassword, to create a user
+export function signUpUser(credentials) {
+  return function(dispatch) {
+    Firebase.auth().createUserWithEmailAndPassword(credentials.email, credentials.password)
+      .then(response => {
+        dispatch(authUser());
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch(authError(error));
+      });
+  }
+}
+
+// signInWithEmailAndPassword, is to log in an existing user.
+export function signInUser(credentials) {
+  return function(dispatch) {
+    Firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(response => {
+        dispatch(authUser());
+      })
+      .catch(error => {
+        dispatch(authError(error));
+      });
+  }
+}
+//////////
+
+
+
 // Action types as constants, passing to our action creators
 // signInUser() and signOutUser() via required type property
 
-export function signInUser() {
-  return {
-    type: SIGN_IN_USER
-  }
-}
+// export function signInUser() {
+//   return {
+//     type: SIGN_IN_USER
+//   }
+// }
 
 export function signOutUser() {
   return {
     type: SIGN_OUT_USER
+  }
+}
+
+// instead of having SIGN_IN_USER,
+// using AUTH_USER, setting authenticated to true for both
+// signing up and logging in.
+export function authUser() {
+  return {
+    type: AUTH_USER
+  }
+}
+
+// if user is not able to sign up or log in and returns an error,
+// authError will catch and in the callback, passing the error
+// message to the new authError action creator.
+export function authError(error) {
+  return{
+  type: AUTH_ERROR,
+  payload: error
   }
 }
