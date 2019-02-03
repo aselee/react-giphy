05a013d8f45627dc869fd29c414b2d9d917b0299
Andrew <32229265+aselee@users.noreply.github.com>
2019-02-03 16:24:16 -0600

    added Gif components/ mstp & mdtp

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,10 +1,59 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as Actions from '../actions';
+import GifList from '../components/GifList';
+import GifModal from '../components/GifModal';
+import '../styles/App.css';
 
 class Favorites extends React.Component {
+  componentWillMount() {
+    this.props.actoins.fetchFavoritedGifs();
+  }
+
   render() {
-    return <div>Favorites</div>;
+    return (
+      <div>
+        <GifList 
+          gifs={ this.props.gifs }
+          onGifSelect={ selectedGif => this.props.actions.openModal({selectedGif}) }
+          onFavoriteSelect={ selectedGif => this.props.actions.favoriteGif({selectedGif}) }
+          onFavoriteDeselect={ selectedGif => this.props.actions.unfavoriteGif({selectedGif}) }
+          isAuthenticated={ this.props.authenticated }
+          isFavorite={true} 
+        />
+        <GifModal 
+          modalIsOpen={ this.props.modalIsOpen }
+          selectedGif={ this.props.selectedGif }
+          onRequestClose={ () => this.props.actions.closeModal()}/>
+      </div>
+    );
   }
 }
 
-export default Favorites;
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated,
+    gifs: state.gifs.favorites,
+    modalIsOpen: state.modal.modalIsOpen,
+    selectedGif: state.modal.selectedGif
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(Actions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
+
+// Before; base template
+// class Favorites extends React.Component {
+//   render() {
+//     return <div>Favorites</div>;
+//   }
+// }
+
+// export default Favorites;
