dfcc2bdf602c4454ee2c213354476d7f84f7d10e
Andrew <32229265+aselee@users.noreply.github.com>
2019-02-03 16:42:18 -0600

    adding fetch fav gifs case

--- src/reducers/gifs.js
+++ src/reducers/gifs.js
@@ -1,11 +1,12 @@
-import { REQUEST_GIFS } from '../actions';
+import { REQUEST_GIFS, FETCH_FAVORITED_GIFS } from '../actions';
 
 const initialState = {
   // setting initiate state for reducer by setting a 
   // data property to an empty array.
   // this helps avoid any issues with gifs coming back as
   // null or undefined. 
-  data: []
+  data: [],
+  favorites: []
 };
 
 // action argument handles any action dispatched in our application.
@@ -19,6 +20,21 @@ export default function gifs(state = initialState, action) {
         // object spread syntax
         ...state, data: action.payload.body.data
       };
+    // Since the gifs are from Giphy, are being returned as an array,
+    // but theyre being returned as a single jSON object from Firebase.
+    // We can however iterate through our Firebase response object
+    // and add it to an empty array, and then return that array from our reducer.
+    case FETCH_FAVORITED_GIFS:
+      var arr = [];
+      for(var i in action.payload) {
+        if (action.payload.hasOwnProperty(i)) {
+          arr.push(action.payload[i]);
+        }
+      }
+      return {
+        ...state, favorites: arr
+      };
+
     default:
       return state;
   }
